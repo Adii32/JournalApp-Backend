3d46976f6e45b1a0b3e487f85b64f4b26e8ac541
Aditya Sharma <142970073+Adii32@users.noreply.github.com>
2025-09-04 17:34:05 +0530

    Update QuotesService.java

--- src/main/java/com/journalapp/service/QuotesService.java
+++ src/main/java/com/journalapp/service/QuotesService.java
@@ -1,5 +1,5 @@
 package com.journalapp.service;
 
 public class QuotesService {
-private static final String apiKey="Xel5hOTyaemQNTMw1IK3wA==I33JCHpkkoWNKG0M";
+private static final String apiKey="{api_key}";
 }
